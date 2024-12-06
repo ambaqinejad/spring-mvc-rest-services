9c79b1415d254aab52005e61cf93755ad809fce2
ambaqinejad <ambaqinejad@gamil.com>
2024-12-06 15:09:11 +0330

    get by id

--- src/main/java/ir/ambaqinejad/springmvcrestservices/controller/ProductController.java
+++ src/main/java/ir/ambaqinejad/springmvcrestservices/controller/ProductController.java
@@ -3,13 +3,16 @@ package ir.ambaqinejad.springmvcrestservices.controller;
 import ir.ambaqinejad.springmvcrestservices.model.Product;
 import ir.ambaqinejad.springmvcrestservices.service.ProductService;
 import org.slf4j.Logger;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.UUID;
 
 @RestController
+@RequestMapping("/api/v1/products")
 public class ProductController {
     private static final Logger log = org.slf4j.LoggerFactory.getLogger(ProductController.class);
     private ProductService productService;
@@ -18,13 +21,14 @@ public class ProductController {
         this.productService = productService;
     }
 
-    @RequestMapping("/api/v1/products")
+    @RequestMapping(method = RequestMethod.GET)
     public List<Product> getAllProducts() {
         return this.productService.getAllProducts();
     }
 
-    public Product getProductById(UUID id) {
-        log.debug("Get product by id in controller. id: " + id);
+    @RequestMapping(value = "/{productId}", method = RequestMethod.GET)
+    public Product getProductById(@PathVariable("productId") UUID id) {
+        log.debug("Get product by id in controller. id: " + id + ".");
         return productService.getProductById(id);
     }
 }
