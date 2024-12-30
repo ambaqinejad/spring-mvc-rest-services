c258b5dd4bb20a984467c4604d260bc9e032a6ee
ambaqinejad <ambaqinejad@gamil.com>
2024-12-30 20:09:14 +0330

    Return Data With Mockito

--- src/test/java/ir/ambaqinejad/springmvcrestservices/controller/ProductControllerTest.java
+++ src/test/java/ir/ambaqinejad/springmvcrestservices/controller/ProductControllerTest.java
@@ -1,6 +1,8 @@
 package ir.ambaqinejad.springmvcrestservices.controller;
 
+import ir.ambaqinejad.springmvcrestservices.model.Product;
 import ir.ambaqinejad.springmvcrestservices.service.ProductService;
+import ir.ambaqinejad.springmvcrestservices.service.ProductServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
@@ -10,12 +12,18 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.UUID;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = ProductController.class)
 class ProductControllerTest {
+
+    ProductServiceImpl productServiceImpl = new ProductServiceImpl();
+
 
     @Autowired
     MockMvc mockMvc;
@@ -32,9 +40,12 @@ class ProductControllerTest {
 
     @Test
     void getProductById() throws Exception {
+        Product product = productServiceImpl.getAllProducts().get(0);
+        given(productService.getProductById(any(UUID.class))).willReturn(product);
         mockMvc.perform(get("/api/v1/products/" + UUID.randomUUID())
                 .accept(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON));
     }
 
     @Test
