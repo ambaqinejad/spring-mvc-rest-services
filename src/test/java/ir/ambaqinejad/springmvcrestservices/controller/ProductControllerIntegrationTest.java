91b54cfbd913271cff608670051b23004366d538
ambaqinejad <ambaqinejad@gamil.com>
2025-01-15 16:58:32 +0330

    get product by id test

--- src/test/java/ir/ambaqinejad/springmvcrestservices/controller/ProductControllerIntegrationTest.java
+++ src/test/java/ir/ambaqinejad/springmvcrestservices/controller/ProductControllerIntegrationTest.java
@@ -1,14 +1,17 @@
 package ir.ambaqinejad.springmvcrestservices.controller;
 
+import ir.ambaqinejad.springmvcrestservices.entity.Product;
 import ir.ambaqinejad.springmvcrestservices.model.ProductDTO;
 import ir.ambaqinejad.springmvcrestservices.repository.ProductRepository;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -20,6 +23,7 @@ class ProductControllerIntegrationTest {
     ProductController productController;
     @Autowired
     ProductRepository productRepository;
+
     @Test
     void createProduct() {
     }
@@ -42,6 +46,17 @@ class ProductControllerIntegrationTest {
 
     @Test
     void getProductById() {
+        Product product = productRepository.findAll().get(0);
+        ProductDTO productDTO = productController.getProductById(product.getId());
+        assertThat(productDTO).isNotNull();
+        assertThat(productDTO.getName()).isEqualTo(product.getName());
+    }
+
+    @Test
+    void getProductByIdNotFoundException() {
+        assertThrows(NotFoundException.class, () -> {
+            productController.getProductById(UUID.randomUUID());
+        });
     }
 
     @Test
